b43073fbd69fe7513df5e619f26bf597132624a8
Trilgon <65063208+Trilgon@users.noreply.github.com>
2020-12-02 15:39:45 +0300

    Update commit

--- src/Deque/Sorts.java
+++ src/Deque/Sorts.java
@@ -1,21 +1,74 @@
 package Deque;
 
 public class Sorts {
-    public  Deque<Integer> sortD(Deque<Integer> deque){
-        if (!deque.isEmpty() || deque.size() != 1){
-            Deque<Integer> packOne = new Deque<Integer>();
-            Deque<Integer> packTwo = new Deque<Integer>();
+    public static Deque<Integer> sortD(Deque<Integer> deque) {
+        if (!deque.isEmpty() || deque.size() != 1) {
+            Deque<Object> packOne = new Deque<>();
+            Deque<Object> packTwo = new Deque<>();
             Integer temp;
-            for (int i = 0; i < deque.size(); i++){
+            boolean isPackOne = true;
+            boolean wasSort = true;
+            packOne.pushRight(deque.peekLeft());
+            for (int i = deque.size(); i > 1; i--) {
                 temp = deque.popLeft();
-                if (temp <= deque.peekLeft()){
-                    packOne.pushRight(temp);
-                    packOne.pushRight(deque.popLeft());
-                } else {
-                    packOne.pushRight(temp);
-                    packTwo.pushRight(deque.popLeft());
+                if (temp <= deque.peekLeft() && isPackOne) {
+                    packOne.pushRight(deque.peekLeft());
+                } else if (temp > deque.peekLeft() && isPackOne) {
+                    packTwo.pushRight(deque.peekLeft());
+                    isPackOne = false;
+                    wasSort = false;
+                    packOne.pushRight(';');
+                } else if (temp <= deque.peekLeft() && !isPackOne) {
+                    packTwo.pushRight(deque.peekLeft());
+                } else if (temp > deque.peekLeft() && !isPackOne) {
+                    packOne.pushRight(deque.peekLeft());
+                    isPackOne = true;
+                    packTwo.pushRight(';');
                 }
             }
+            deque.popLeft();
+            if (!wasSort) {
+                if (!packOne.peekRight().equals(';')) {
+                    packOne.pushRight(';');
+                } else {
+                    packTwo.pushRight(';');
+                }
+            }
+                while (!packOne.isEmpty() && !packTwo.isEmpty()) {
+                    if (!packOne.peekLeft().equals(';') && !packTwo.peekLeft().equals(';')) {
+                        if ((Integer) packOne.peekLeft() <= (Integer) packTwo.peekLeft()) {
+                            deque.pushRight((Integer) packOne.popLeft());
+                        } else {
+                            deque.pushRight((Integer) packTwo.popLeft());
+                        }
+                    } else if (packOne.peekLeft().equals(';') && !packTwo.peekLeft().equals(';')) {
+                        deque.pushRight((Integer) packTwo.popLeft());
+                    } else if (packTwo.peekLeft().equals(';') && !packOne.peekLeft().equals(';')) {
+                        deque.pushRight((Integer) packOne.popLeft());
+                    } else if (packOne.peekLeft().equals(';') && packTwo.peekLeft().equals(';')){
+                        packOne.popLeft();
+                        packTwo.popLeft();
+                    }
+                }
+                if (packOne.isEmpty() && !packTwo.isEmpty()){
+                    while (!packTwo.isEmpty()){
+                        if (!packTwo.peekLeft().equals(';')){
+                            deque.pushRight((Integer) packTwo.popLeft());
+                        } else if(packTwo.peekLeft().equals(';')){
+                            packTwo.popLeft();
+                        }
+                    }
+                } else if (!packOne.isEmpty() && packTwo.isEmpty()){
+                    while (!packOne.isEmpty()){
+                        if (!packOne.peekLeft().equals(';')){
+                            deque.pushRight((Integer) packOne.popLeft());
+                        } else if(packOne.peekLeft().equals(';')){
+                            packOne.popLeft();
+                        }
+                    }
+                }
+                if (!wasSort)
+                deque = sortD(deque);
         }
         return deque;
     }
